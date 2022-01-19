2e891c9346d1f2f8638370a82c73a9b5f47f4ff3
ScottyLiss <scottlissone@gmail.com>
2022-01-19 09:37:41 +0000

    Pre Section 27

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,8 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
+            
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -59,6 +61,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
